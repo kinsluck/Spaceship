17d4a044865bade792d1d0aeadc2b848bd0fdfc0
kins <kins36936@gmail.com>
2018-04-10 15:43:59 +0800

    Initial commit

---
+++ ship.py
@@ -0,0 +1,26 @@
+import pygame
+
+
+class Ship():
+    def __init__(self, screen):
+        """Initialize the ship, and set its starting position."""
+        self.screen = screen
+        # Load the ship image, and get its rect.
+        self.image = pygame.image.load('F:\\code\\pygame\\images\\ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        # Start each new ship at the bottom center of the screen.
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+        self.moving_right = False
+        self.moving_left = False
+
+    def biltme(self):
+        self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        if self.moving_right:
+            self.rect.centerx += 1
+        if self.moving_left:
+            self.rect.centerx -= 1
