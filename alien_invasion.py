1c4c59d76f6a4b87719070a3b738790d59d81bfa
kins <kins36936@gmail.com>
2018-04-24 08:33:15 +0800

    Create fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -16,14 +16,13 @@ def run_game():
     ship = Ship(ai_settings, screen)
     bullets = Group()
     aliens = Group()
-    gf.create_fleet(ai_settings, screen, aliens)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
-        gf.create_fleet(ai_settings, screen, ship, aliens)
 
 
 run_game()
